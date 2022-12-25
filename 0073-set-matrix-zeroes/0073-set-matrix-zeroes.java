1f58cf8dcd99afeadd57674ec4c234bebaea8e93
Subasishsahoo <118912106+Subasishsahoo@users.noreply.github.com>
2022-12-25 21:49:49 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (90.78%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,7 +2,7 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean firstRow = false, firstCol = false;
     for (int i = 0; i < matrix.length; i++)
-      for (int j = 0; j < matrix[0].length; j++)
+      for (int j = 0; j < matrix[i].length; j++)
         if (matrix[i][j] == 0) {
           if (i == 0) firstRow = true;
           if (j == 0) firstCol = true;
